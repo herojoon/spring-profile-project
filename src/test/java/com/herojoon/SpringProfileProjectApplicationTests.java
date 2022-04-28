1e926b493665e5fe621017c83e75cba323ef199d
herojoon <herojoon@gmail.com>
2022-04-29 02:33:37 +0900

    create project

---
+++ src/test/java/com/herojoon/SpringProfileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herojoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfileProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
